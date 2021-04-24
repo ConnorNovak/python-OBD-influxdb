21eb448fbb0132262f245c2b2312679671fb9a14
Connor Novak <connorrnovak@gmail.com>
2021-04-23 22:20:41 -0700

    Set up initial repo structure, meta-files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # python-OBD-influxdb
-Integration of python-OBD package with InfluxDB time-series database
+A python module for integrating the `python-OBD` module with the InfluxDB time-series database.
+
+## Installation
+```
+$ python -m pip install obd-influxdb
+```
+
+## Basic Usage
+```
+import obd-influxdb
+
+#TODO (connor): write basic example
+```
+
+## References
+
+- Github: [python-obd](https://github.com/brendan-w/python-OBD)
+- Github: [influxdb-client-python](https://github.com/brendan-w/python-OBD)
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-OBD-influxdb",
+    version="0.0.1",
+    author="Connor Novak",
+    author_email="connor.r.novak@gmail.com",
+    description="Python module for logging obd data to InfluxDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Ubuntu",
+    ],
+    install_requires=[
+        'influxdb-client',
+        'numpy',
+        'obd',
+        'plac',
+    ],
+    python_requires='>=3.6',
+)
